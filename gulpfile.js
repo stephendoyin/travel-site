45a409cb81328d0347ce215cd37f3cad2bd51189
Stephen <stevedoyin@gmail.com>
2018-05-20 02:15:48 +0100

    general changes

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('default', function () {
+    console.log("Hooray - you created a Gulp task.");
+});
+
+gulp.task('html', function () {
+    console.log("Imagine something useful being done to your HTML here.");
+});
+
+gulp.task('styles', function () {
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function () {
+
+    browserSync.init({
+        server:{
+            baseDir: "app"
+        }
+    });
+
+    watch('./app/index.html', function () {
+        browserSync.reload();
+    });
+
+    watch('./app/assets/styles/**/*.css', function () {
+        gulp.start('styles');
+    });
+
+});
